46c1a18c79e4ea891676c5dadde62ddc1b078ffc
Susie6 <201830582210@mail.scut.edu.cn>
2019-05-01 23:31:59 +0800

    css

--- js/animation.js
+++ js/animation.js
@@ -1,7 +1,8 @@
-var loadingPage = document.getElementById("loadingPage");
 var startingPage = document.getElementById("startingPage");
 var mainPage = document.getElementById("mainPage");
-var mainPageTitle = document.getElementById("mainPageTitle");
+
+var btn = document.getElementById("activities");
+//导航栏
 var activities = {
     timeCapsule: document.getElementById("timeCapsule"),
     wishDream: document.getElementById("wishDream"),
@@ -9,26 +10,71 @@ var activities = {
     creativeMarket: document.getElementById("creativeMarket"),
     timeStation: document.getElementById("timeStation"),
 };
-var sectionPages = {
-    timeCapsulePage: document.getElementById("timeCapsulePage"),
-    wishDreamPage: document.getElementById("wishDreamPage"),
-    photoWallPage: document.getElementById("photoWallPage"),
-    creativeMarketPage: document.getElementById("creativeMarketPage"),
-    timeStationPage: document.getElementById("timeStationPage"),
-};
-var goBack = document.getElementById("goBack");
+//活动分页面
+var timeCapsule = document.getElementById("timeCapsulePage");
+var wishDream = document.getElementById("wishDreamPage");
+var photoWall = document.getElementById("photoWallPage");
+var creativeMarket = document.getElementById("creativeMarketPage");
+var timeStation = document.getElementById("timeStationPage");
+//返回键
+var photoWall_back = document.getElementById("photoWall_back");
+var creativeMarket_back = document.getElementById("creativeMarket_back");
+var timeCapsule_back = document.getElementById("timeCapsule_back");
+var timeStation_back = document.getElementById("timeStation_back");
+var wishDream_back = document.getElementById("wishDream_back");
+//跳转
 var go = {
     goTimeCapsule: document.getElementById("goTimeCapsule"),
     goWishDream: document.getElementById("goWishDream"),
     goPhotoWall: document.getElementById("goPhotoWall"),
-    goCreativeMarket: document.getElementById("goCreativeMarket"),
-    goTimeStation: document.getElementById("goTimeStation"),
 };
-activities.addEventListener("click", showPage);
-goBack.addEventListener("click", hidePage);
-var loading = setTimeout(startAnimation, 15000);
-window.addEventListener("load", function () {
-    loadingPage.classList.add("fadeOut");
-    setTimeout(startAnimation, 2000);
-    clearTimeout(loading);
-})
+
+photoWall_back.addEventListener("click", hidePhotoWall);
+creativeMarket_back.addEventListener("click", hideCreateMarket);
+timeCapsule_back.addEventListener("click", hideTimeCapsule);
+timeStation_back.addEventListener("click", hideTimeStation);
+wishDream_back.addEventListener("click", hideWishDream);
+
+btn.addEventListener("click",show);
+
+function show(event) {
+    var click = event.target.id;
+    var show_this = document.getElementById(click+"Page");
+    console.log(show_this);
+    mainPage.classList.remove("show");
+    mainPage.classList.add("hide");
+    show_this.classList.remove("hide");
+    show_this.classList.add("show");
+}
+
+function hidePhotoWall() {
+    mainPage.classList.remove("hide");
+    mainPage.classList.add("show");
+    photoWall.classList.remove("show");
+    photoWall.classList.add("hide");
+}
+
+function hideCreateMarket(){
+    mainPage.classList.remove("hide");
+    mainPage.classList.add("show");
+    creativeMarket.classList.remove("show");
+    creativeMarket.classList.add("hide");
+}
+function hideTimeCapsule(){
+    mainPage.classList.remove("hide");
+    mainPage.classList.add("show");
+    timeCapsule.classList.remove("show");
+    timeCapsule.classList.add("hide");
+}
+function hideTimeStation(){
+    mainPage.classList.remove("hide");
+    mainPage.classList.add("show");
+    timeStation.classList.remove("show");
+    timeStation.classList.add("hide");
+}
+function hideWishDream(){
+    mainPage.classList.remove("hide");
+    mainPage.classList.add("show");
+    wishDream.classList.remove("show");
+    wishDream.classList.add("hide");
+}
